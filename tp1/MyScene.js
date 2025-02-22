249288ee39ed7fb120b966aaa464b0d3790d68cc
Sofia Gonçalves <up202205020@edu.fe.up.pt>
2025-02-22 16:29:05 +0000

    Add: MyTriangleBig and screenshots

--- tp1/MyScene.js
+++ tp1/MyScene.js
@@ -3,6 +3,7 @@ import { MyDiamond } from "./MyDiamond.js";
 import { MyTriangle } from "./MyTriangle.js";
 import { MyParallelogram } from "./MyParallelogram.js";
 import { MyTriangleSmall } from "./MyTriangleSmall.js";
+import { MyTriangleBig } from "./MyTriangleBig.js";
 
 /**
  * MyScene
@@ -32,13 +33,13 @@ export class MyScene extends CGFscene {
     this.triangle = new MyTriangle(this);
     this.parallelogram = new MyParallelogram(this);
     this.triangleSmall = new MyTriangleSmall(this);
+    this.triangleBig = new MyTriangleBig(this);
 
     //Objects connected to MyInterface
     this.displayAxis = true;
     this.showTriangle = true;
     this.showDiamond = true;
     this.showParallelogram = true;
-    this.showTriangleSmall = true;
     this.scaleFactor = 1;
   }
   initLights() {
@@ -104,7 +105,8 @@ export class MyScene extends CGFscene {
     if(this.showDiamond) this.diamond.display();
     if(this.showTriangle) this.triangle.display();
     if(this.showParallelogram) this.parallelogram.display();
-    this.triangleSmall.display();
+    // this.triangleSmall.display();
+    // this.triangleBig.display();
 
     // ---- END Primitive drawing section
   }
