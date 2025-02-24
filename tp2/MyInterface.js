fb32c95ea4f8fe4aabe23157ecbdc895aa675da3
Diana Nunes <up202208247@edu.fe.up.pt>
2025-02-24 16:46:49 +0000

    Add: Tangram

---
+++ tp2/MyInterface.js
@@ -0,0 +1,31 @@
+import {CGFinterface, dat} from '../lib/CGF.js';
+
+/**
+* MyInterface
+* @constructor
+*/
+export class MyInterface extends CGFinterface {
+    constructor() {
+        super();
+    }
+
+    init(application) {
+        // call CGFinterface init
+        super.init(application);
+        
+        // init GUI. For more information on the methods, check:
+        // https://github.com/dataarts/dat.gui/blob/master/API.md
+        this.gui = new dat.GUI();
+
+        //Checkbox element in GUI
+        this.gui.add(this.scene, 'displayAxis').name('Display Axis');
+        this.gui.add(this.scene, 'showDiamond').name('show Diamond');
+        this.gui.add(this.scene, 'showTriangle').name('show Triangle');
+        this.gui.add(this.scene, 'showParallelogram').name('show Parallelogram');
+
+        //Slider element in GUI
+        this.gui.add(this.scene, 'scaleFactor', 0.1, 5).name('Scale Factor');
+
+        return true;
+    }
+}
